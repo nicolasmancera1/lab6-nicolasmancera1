815d84abe3260c6f7ec55b13866a0e03ad868a88
Nicolas Mancera <nicolasmava@unisabana.edu.co>
2026-04-07 21:22:58 -0500

    lab2 exercise3

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,14 @@
 # Write your code here!
+def employee_print(employee_info):
+    temp = employee_info.copy()
+    name = temp.pop("Name", "N/A")
+    print(f"Name: {name}")
+    salary = temp.pop("Salary", "N/A")
+    print(f"Salary: {salary}")
+    role = temp.pop("Role", "N/A")
+    print(f"Role: {role}")
+    if not temp:
+        print("No other info!")
+    else:
+        for key, value in temp.items():
+            print(f"{key}: {value}")
